b649da1461563dfd7b085ab4a9d7927fdf333a3c
marta <Akademskig@gmail.com>
2019-03-04 14:10:17 +0100

    Add pianoControllerProvider

---
+++ client/src/components/PianoController.js
@@ -0,0 +1,150 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types"
+import { Paper, Grid, FormControl, InputLabel, Select, MenuItem, Switch, FormControlLabel, Button } from "@material-ui/core";
+import { withStyles } from '@material-ui/core/styles';
+import { MidiNumbers } from 'react-piano';
+
+const styles = theme => ({
+    controllerGrid: {
+        marginBottom: "20px"
+    },
+    controlPaper: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'left',
+        color: theme.palette.text.secondary,
+    },
+    backgroundDiv: {
+        padding: theme.spacing.unit * 2,
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        flexGrow: 1,
+        margin: "auto",
+        width: "80%"
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+    },
+});
+
+class PianoController extends Component {
+
+    render() {
+        const {
+            classes,
+            onClickPlay,
+            onClickClear,
+            onClickStop,
+            onClickSave,
+            onClickUndo,
+            onChangeFirstNote,
+            onChangeLastNote,
+            instrumentName,
+            instrumentList,
+            toggleRecording,
+            recordingOn,
+            onChangeInstrument,
+            noteRange } = this.props;
+        const midiNumbersToNotes = MidiNumbers.NATURAL_MIDI_NUMBERS.reduce((obj, midiNumber) => {
+            obj[midiNumber] = MidiNumbers.getAttributes(midiNumber).note;
+            return obj;
+        }, {});
+
+        return (
+            <div className={classes.backgroundDiv}>
+                <Grid container spacing={24} className={classes.controllerGrid}>
+                    <Grid item xs={6}>
+                        <Paper className={classes.controlPaper}>
+                            <form className={classes.root} >
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel htmlFor="first-note">Start Note</InputLabel>
+                                    <Select
+                                        value={noteRange.first}
+                                        name="first-note"
+                                        onChange={onChangeFirstNote}
+                                        inputProps={{
+                                            name: 'first-note',
+                                            id: 'first-note',
+                                        }}
+                                    >
+                                        {
+                                            MidiNumbers.NATURAL_MIDI_NUMBERS.map((midiNumber) =>
+                                                <MenuItem value={midiNumber} disabled={midiNumber >= noteRange.last} key={midiNumber}> {midiNumbersToNotes[midiNumber]}</MenuItem>
+                                            )}
+                                    </Select>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel>End Note</InputLabel>
+                                    <Select
+                                        onChange={onChangeLastNote}
+                                        value={noteRange.last}>
+                                        {MidiNumbers.NATURAL_MIDI_NUMBERS.map((midiNumber) => (
+                                            <MenuItem value={midiNumber} disabled={midiNumber <= noteRange.first} key={midiNumber}> {midiNumbersToNotes[midiNumber]}</MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                                <FormControl className={classes.formControl}>
+                                    <InputLabel>Instrument</InputLabel>
+                                    <Select
+                                        onChange={onChangeInstrument}
+                                        value={instrumentName}>
+                                        {instrumentList && instrumentList.map((value) => (
+                                            <MenuItem value={value} key={value}>
+                                                {value}
+                                            </MenuItem>
+                                        ))}
+                                    </Select>
+                                </FormControl>
+                            </form>
+                        </Paper>
+
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Paper className={classes.controlPaper}>
+                            <FormControlLabel
+                                className={classes.formControl}
+                                label={!recordingOn ? "START RECORDING" : "STOP RECORDING"}
+                                control={<Switch
+                                    onChange={toggleRecording}
+                                    checked={recordingOn}
+                                    value={recordingOn}
+                                />}>
+
+                            </FormControlLabel>
+                            <div className="mt-5">
+                                <Button onClick={onClickPlay}>Play</Button>
+                                <Button onClick={onClickStop}>Stop</Button>
+                                <Button onClick={onClickClear}>Clear</Button>
+                                <Button onClick={onClickUndo}>Undo</Button>
+                                <Button onClick={onClickSave}>Save</Button>
+                            </div>
+                            {/* <div className="mt-5">
+                                <strong>Recorded notes</strong>
+                                <div>{JSON.stringify(recording)}</div>
+                            </div> */}
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(PianoController)
+
+PianoController.propTypes = {
+    classes: PropTypes.object,
+    onClickPlay: PropTypes.func,
+    onClickClear: PropTypes.func,
+    onClickStop: PropTypes.func,
+    onClickSave: PropTypes.func,
+    onClickUndo: PropTypes.func,
+    onChangeFirstNote: PropTypes.func,
+    onChangeLastNote: PropTypes.func,
+    instrumentName: PropTypes.string,
+    instrumentList: PropTypes.arrayOf(PropTypes.string),
+    toggleRecording: PropTypes.func,
+    recordingOn: PropTypes.bool,
+    onChangeInstrument: PropTypes.func,
+    noteRange: PropTypes.object
+}
---
+++ client/src/providers/PianoControllerProvider.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import { saveMidi } from "../services/midiService"
+import _ from 'lodash';
+import InstrumentListWrapper from './InstrumentListProvider';
+
+class PianoControllerProvider extends React.Component {
+
+    state = {
+        noteRange: {
+            first: 43,
+            last: 67,
+        },
+        instrumentName: "acoustic_grand_piano",
+        recording: {
+            mode: 'NOT_RECORDING',
+            events: [],
+            currentTime: 0,
+            currentEvents: [],
+        },
+        recordingOn: false,
+    }
+    scheduledEvents = [];
+
+    getRecordingEndTime = () => {
+        if (this.state.recording.events.length === 0) {
+            return 0;
+        }
+        return Math.max(
+            ...this.state.recording.events.map(event => event.time + event.duration),
+        );
+    };
+
+    setRecording = value => {
+        this.setState({
+            recording: Object.assign({}, this.state.recording, value),
+        });
+    };
+
+    onClickPlay = () => {
+        this.setRecording({
+            mode: 'PLAYING',
+        });
+        this.setState({ recordingOn: false })
+        const startAndEndTimes = _.uniq(
+            _.flatMap(this.state.recording.events, event => [
+                event.time,
+                event.time + event.duration,
+            ]),
+        );
+
+        startAndEndTimes.forEach((time, i) => {
+            this.scheduledEvents.push(
+                setTimeout(() => {
+                    const currentEvents = this.state.recording.events.filter(event => {
+                        return event.time <= time && event.time + event.duration > time
+                    });
+                    this.setRecording({
+                        currentEvents,
+                    });
+                }, time * 1000),
+            );
+        });
+        // Stop at the end
+        setTimeout(() => {
+            this.onClickStop();
+        }, this.getRecordingEndTime() * 1000);
+    };
+
+    onClickStop = () => {
+        this.scheduledEvents.forEach(scheduledEvent => {
+            clearTimeout(scheduledEvent);
+        });
+        this.setRecording({
+            mode: "NOT_RECORDING",
+            currentEvents: [],
+        });
+        this.setState({ recordingOn: false })
+    };
+
+    onClickClear = () => {
+        this.onClickStop();
+        this.setRecording({
+            mode: "NOT_RECORDING",
+            events: [],
+            currentEvents: [],
+            currentTime: 0,
+        });
+        this.setState({ recordingOn: false })
+    };
+
+    onClickUndo = () => {
+        if (this.state.recording.events.length === 0) {
+            return
+        }
+        const eventsLength = this.state.recording.events.length
+        this.setRecording({
+            mode: "NOT_RECORDING",
+            events: this.state.recording.events.slice(0, eventsLength - 1),
+            currentTime: this.state.recording.events[eventsLength - 1].time + this.state.recording.events[eventsLength - 1].duration
+        })
+        this.setState({
+            recordingOn: false
+        })
+    }
+    onClickSave = () => {
+        console.log(this.props.instrumentList)
+        const instrumentIndex = this.props.instrumentList.findIndex(i => i === this.state.instrumentName)
+        saveMidi(this.state.recording.events, instrumentIndex)
+    }
+    onChangeFirstNote = (event) => {
+        this.setState({
+            noteRange: {
+                first: parseInt(event.target.value, 10),
+                last: this.state.noteRange.last,
+            },
+        });
+    };
+
+    onChangeLastNote = (event) => {
+        this.setState({
+            noteRange: {
+                first: this.state.noteRange.first,
+                last: parseInt(event.target.value, 10),
+            },
+        });
+    };
+    onChangeInstrument = (event) => {
+        this.setState({
+            instrumentName: event.target.value,
+        });
+    };
+
+    toggleRecording = (event, checked) => {
+        if (checked) {
+            this.setRecording({
+                mode: 'RECORDING',
+            });
+        }
+        else {
+            this.setRecording({
+                mode: 'NOT_RECORDING',
+            });
+        }
+        this.setState({
+            recordingOn: checked,
+            paused: true
+        })
+    }
+
+    render() {
+        const propsToPass = {
+            toggleRecording: this.toggleRecording,
+            onChangeInstrument: this.onChangeInstrument,
+            onChangeFirstNote: this.onChangeFirstNote,
+            onChangeLastNote: this.onChangeLastNote,
+            onClickClear: this.onClickClear,
+            onClickPlay: this.onClickPlay,
+            onClickSave: this.onClickSave,
+            onClickStop: this.onClickStop,
+            onClickUndo: this.onClickUndo,
+            noteRange: this.state.noteRange,
+            recordingOn: this.state.recordingOn,
+            recording: this.state.recording,
+            setRecording: this.setRecording,
+            instrumentName: this.state.instrumentName
+        }
+        return (
+            React.Children.map(this.props.children, (child) => {
+                return React.cloneElement(child, { ...propsToPass });
+            })
+        )
+    }
+}
+
+export default InstrumentListWrapper(PianoControllerProvider)
