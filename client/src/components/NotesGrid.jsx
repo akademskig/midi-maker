8072c6318aacbc2f7a87fb0aa7dc61bc87efe22f
Marta Sušek <marta.susek@uniqcast.com>
2019-03-06 23:46:01 +0100

    Finish add notes funtionality

--- client/src/components/NotesGrid.jsx
+++ client/src/components/NotesGrid.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { MidiNumbers } from 'react-piano';
 import Dimensions from 'react-dimensions';
+import _ from "lodash"
 
 class NotesGrid extends React.Component {
-
     render() {
         const notes = []
         for (let i = this.props.noteRange.first; i <= this.props.noteRange.last; i++)
             notes.push(MidiNumbers.getAttributes(i))
-            notes.reverse()
-            return (
-            <div className="gridContainer" style={{
+        notes.reverse()
+        const canvasContainer = this.refs.canvasContainer
+        return (
+            <div ref="canvasContainer" className="gridContainer" style={{
                 height: window.innerHeight / 2 - 30, bottom: window.innerHeight / 4
             }}>
                 <Canvas
                     midiOffset={this.props.noteRange.first}
-                    currentNotes={this.props.recording}
-                    notes={notes}></Canvas>
+                    recording={this.props.recording}
+                    notes={notes}
+                    canvasContainer={canvasContainer}
+                    setRecording={this.props.setRecording}
+                >
+                </Canvas>
             </div>
         )
     }
@@ -28,65 +33,89 @@ const canvasStyle = {
 }
 let RECT_HEIGHT = 15
 const RECT_WIDTH = 15
-const TOP_OFFSET = 0
 const RECT_SPACE = 0.5
-const RECT_COLOR =  "rgba(4,32,55,1)"
+const RECT_COLOR = "rgba(4,32,55,1)"
 const FIRST_RECT_COLOR = "rgb(255,255,255)"
 const NOTE_COLOR = "#61dafb"
-const START_TIME =window.innerWidth / (RECT_WIDTH+RECT_SPACE)
-const RECT_TIME=4
+const START_TIME = window.innerWidth / (RECT_WIDTH + RECT_SPACE)
+const RECT_TIME = 10
 class Canvas extends React.Component {
+    coordsMap = []
+    eventsRect = []
+    canvasWidth = null
+    lastEvent = {}
+    fontSize = null
+    offsetFirst = null
 
     drawInitial = (canvas) => {
-        let xLength = (this.props.currentNotes.currentTime* RECT_TIME)<START_TIME?START_TIME+3:this.props.currentNotes.currentTime* RECT_TIME+3
+        let xLength = (this.props.recording.currentTime * RECT_TIME) < START_TIME ? START_TIME + 3 : this.props.recording.currentTime * RECT_TIME + 3
         let c = canvas.getContext("2d")
-        canvas.width = (xLength*( RECT_WIDTH+RECT_SPACE))
-        // canvas.height = this.props.notes.length * RECT_HEIGHT + (RECT_SPACE * this.props.notes.length)
-        canvas.height=370
-        RECT_HEIGHT= (canvas.height-RECT_SPACE * this.props.notes.length)/this.props.notes.length
-        let FONT_SIZE= RECT_HEIGHT*0.8
-        let offsetFirst=RECT_WIDTH *FONT_SIZE/7
+        canvas.width = (xLength * (RECT_WIDTH + RECT_SPACE))
+        this.canvasWidth = canvas.width
+        canvas.height = 370
+        RECT_HEIGHT = (canvas.height - RECT_SPACE * this.props.notes.length) / this.props.notes.length
+        this.fontSize = RECT_HEIGHT * 0.8
+        this.offsetFirst = RECT_WIDTH * this.fontSize / 7
+        this.coordsMap = []
         this.props.notes.forEach((n, i) => {
             for (let j = 0; j < xLength; j++) {
-
                 if (j === 0) {
-                    // c.fillStyle = FIRST_RECT_COLOR
-                    // c.fillRect((j * RECT_WIDTH + RECT_SPACE * j), (i * RECT_HEIGHT + RECT_SPACE * i), RECT_WIDTH* FONT_SIZE/7, RECT_HEIGHT);
                     c.fillStyle = "white"
-                    c.font =`${FONT_SIZE}12px Arial`
-                    c.fillText(n.note,3, (RECT_HEIGHT/FONT_SIZE/2 +FONT_SIZE)+i*(RECT_HEIGHT+RECT_HEIGHT/(FONT_SIZE*2.6)))
+                    c.font = `${this.fontSize}12px Arial`
+                    c.fillText(n.note, 3, (RECT_HEIGHT / this.fontSize / 2 + this.fontSize) + i * (RECT_HEIGHT + RECT_HEIGHT / (this.fontSize * 2.6)))
                 }
-                else{
-
+                else {
+                    let x = j * RECT_WIDTH + this.offsetFirst + RECT_SPACE * j
+                    let y = i * RECT_HEIGHT + RECT_SPACE * i
                     c.fillStyle = RECT_COLOR
-                    c.fillRect((j * RECT_WIDTH+offsetFirst  + RECT_SPACE * j), (i * RECT_HEIGHT + RECT_SPACE * i), RECT_WIDTH, RECT_HEIGHT);
+                    c.fillRect(x, y, RECT_WIDTH, RECT_HEIGHT);
+                    this.coordsMap.push({ midiNumber: n.midiNumber, x, y })
                 }
             }
         });
-        // c.fillText(n.note, 1, 12 + i * 16)
-        if(this.props.currentNotes.events==0)
-        return
-        this.props.currentNotes.events.forEach((n, i) => {
-            const x=(RECT_WIDTH* RECT_TIME+Math.floor(n.time * RECT_WIDTH* RECT_TIME)) + offsetFirst
-            const y = Math.floor(canvas.height-((n.midiNumber - this.props.midiOffset) * RECT_HEIGHT + RECT_SPACE * (n.midiNumber - this.props.midiOffset)))-(RECT_HEIGHT+RECT_SPACE)
-            const width=Math.floor(n.duration*RECT_WIDTH*RECT_TIME)
+        if (this.props.recording.events.length > 0) {
+            this.props.recording.events.forEach((n, i) => {
+                const x = RECT_WIDTH + Math.floor(n.time * RECT_WIDTH * RECT_TIME) + this.offsetFirst
+                const y = Math.floor(canvas.height - ((n.midiNumber - this.props.midiOffset) * RECT_HEIGHT + RECT_SPACE * (n.midiNumber - this.props.midiOffset))) - (RECT_HEIGHT + RECT_SPACE)
+                const width = Math.floor(n.duration * RECT_WIDTH * RECT_TIME)
+                c.fillStyle = NOTE_COLOR
+                c.clearRect(x, y, width, RECT_HEIGHT);
+                c.fillRect(x, y, width, RECT_HEIGHT);
+            });
+        }
+        if (this.eventsRect.length === 0) {
+            return
+        }
+        this.eventsRect.forEach(eventRect => {
             c.fillStyle = NOTE_COLOR
-            c.clearRect(x, y, width, RECT_HEIGHT);
-            c.fillRect(x, y, width, RECT_HEIGHT);
-        });
+            c.clearRect(eventRect.x, eventRect.y, RECT_WIDTH, RECT_HEIGHT);
+            c.fillRect(eventRect.x, eventRect.y, RECT_WIDTH, RECT_HEIGHT);
+        })
     }
 
     showCoords = (event) => {
-        var x = event.clientX - 10;
-        var y = event.clientY - 10;
-        var coords = "X coordinates: " + x + ", Y coordinates: " + y;
+        var x = event.clientX + this.props.canvasContainer.scrollLeft
+        var y = event.clientY - 240;
+        const rect = this.coordsMap.find(i =>
+            (x >= i.x && x <= (i.x + RECT_WIDTH)) && (y >= i.y && y <= (i.y + RECT_HEIGHT))
+        )
+        if (!rect || this.props.recording.mode !== "RECORDING")
+            return
+        const lastEvent = {
+            midiNumber: rect.midiNumber,
+            time: (rect.x - (RECT_WIDTH) - this.offsetFirst) / RECT_WIDTH / RECT_TIME,
+            duration: 1 / RECT_TIME
+        }
+        this.props.setRecording({
+            events: this.props.recording.events.concat(lastEvent),
+        })
+        const canvas = this.refs.canvas
+        this.drawInitial(canvas)
     }
     componentWillReceiveProps(newProps) {
         const canvas = this.refs.canvas
-        // this.drawInitial(canvas)
-        if (newProps.currentNotes.events.length !== this.props.currentNotes.events.length) {
+        if (newProps.recording.events.length !== this.props.recording.events.length) {
             this.drawInitial(canvas)
-
         }
     }
     componentDidUpdate() {
@@ -96,7 +125,7 @@ class Canvas extends React.Component {
     render() {
         return (
             <div>
-                <canvas ref="canvas" style={canvasStyle} onClick={this.showCoords} />
+                <canvas id="canvas" ref="canvas" style={canvasStyle} onClick={this.showCoords} />
             </div>
         )
     }
