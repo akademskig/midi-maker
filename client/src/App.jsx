af9cb70a20a539e70e639cc87466d3b797540e0e
Marta Sušek <marta.susek@uniqcast.com>
2019-03-16 19:38:18 +0100

    add mui theme

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,10 +3,32 @@ import { BrowserRouter } from 'react-router-dom'
 import { MainRouter } from './MainRouter';
 import "./styles/css/index.css"
 
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#757ce8',
+      main: '#3f50b5',
+      dark: '#002884',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ff7961',
+      main: '#f44336',
+      dark: '#ba000d',
+      contrastText:  '#fff',
+    },
+  }
+});
+
 const App = () => (
+  <MuiThemeProvider theme={theme}>
   <BrowserRouter>
       <MainRouter/>
   </BrowserRouter>
+  </MuiThemeProvider>
 )
 
 export default App;
